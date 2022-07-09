2f3e135e2a2b476abcc0f13e9f85b6d3caa23612
Andrey Kudlay <matroskin8@i.ua>
2022-07-09 13:03:33 +0300

    Save slide

--- admin/class-wfmpanel-admin.php
+++ admin/class-wfmpanel-admin.php
@@ -5,6 +5,35 @@ class Wfmpanel_Admin {
 	public function __construct() {
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts_styles' ) );
 		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+		add_action( 'admin_post_save_slide', array( $this, 'save_slide' ) );
+	}
+
+	public static function debug( $data ) {
+		echo "<pre>" . print_r( $data, 1 ) . "</pre>";
+	}
+
+	public function save_slide() {
+		if ( ! isset( $_POST['wfmpanel_add_slide'] ) || ! wp_verify_nonce( $_POST['wfmpanel_add_slide'], 'wfmpanel_action' ) ) {
+			wp_die( __( 'Error!', 'wfmpanel' ) );
+		}
+
+		$slide_title   = isset( $_POST['slide_title'] ) ? trim( $_POST['slide_title'] ) : '';
+		$slide_content = isset( $_POST['slide_content'] ) ? trim( $_POST['slide_content'] ) : '';
+		if ( empty( $slide_title ) || empty( $slide_content ) ) {
+			set_transient( 'wfmpanel_form_errors', __( 'Form fields are required', 'wfmpanel' ), 30 );
+		} else {
+			$slide_title   = wp_unslash( $slide_title );
+			$slide_content = wp_unslash( $slide_content );
+			global $wpdb;
+			if ( $wpdb->query( $wpdb->prepare(
+				"INSERT INTO wfm_panel (title, content) VALUES (%s, %s)", $slide_title, $slide_content
+			) ) ) {
+				set_transient( 'wfmpanel_form_success', __( 'Slide added', 'wfmpanel' ), 30 );
+			} else {
+				set_transient( 'wfmpanel_form_errors', __( 'Error saving slide', 'wfmpanel' ), 30 );
+			}
+		}
+		wp_redirect( $_POST['_wp_http_referer'] );
 	}
 
 	public function enqueue_scripts_styles() {
@@ -13,10 +42,16 @@ class Wfmpanel_Admin {
 	}
 
 	public function admin_menu() {
-		add_menu_page( __( 'WFM Panel Main', 'wfmpanel' ), __( 'WFM Panel', 'wfmpanel' ), 'manage_options', 'wfmpanel-main', array( $this, 'render_main_page' ), 'dashicons-embed-photo' );
+		add_menu_page( __( 'WFM Panel Main', 'wfmpanel' ), __( 'WFM Panel', 'wfmpanel' ), 'manage_options', 'wfmpanel-main', array(
+			$this,
+			'render_main_page'
+		), 'dashicons-embed-photo' );
 
 		add_submenu_page( 'wfmpanel-main', __( 'WFM Panel Main', 'wfmpanel' ), __( 'Set slide', 'wfmpanel' ), 'manage_options', 'wfmpanel-main' );
-		add_submenu_page( 'wfmpanel-main', __( 'Slides management', 'wfmpanel' ), __( 'Slides management', 'wfmpanel' ), 'manage_options', 'wfmpanel-slides', array( $this, 'render_slides_page' ) );
+		add_submenu_page( 'wfmpanel-main', __( 'Slides management', 'wfmpanel' ), __( 'Slides management', 'wfmpanel' ), 'manage_options', 'wfmpanel-slides', array(
+			$this,
+			'render_slides_page'
+		) );
 	}
 
 	public function render_main_page() {
@@ -30,11 +65,11 @@ class Wfmpanel_Admin {
 	public static function get_posts() {
 
 		return new WP_Query( array(
-			'post_type' => 'post',
+			'post_type'      => 'post',
 			'posts_per_page' => 10,
-			'orderby' => 'ID',
-			'order' => 'DESC',
-			'paged' => $_GET['paged'] ?? 1,
+			'orderby'        => 'ID',
+			'order'          => 'DESC',
+			'paged'          => $_GET['paged'] ?? 1,
 		) );
 	}
 
--- admin/templates/slides-page-template.php
+++ admin/templates/slides-page-template.php
@@ -1,4 +1,63 @@
 <div class="wrap">
     <h1><?php _e( 'Slides management', 'wfmpanel' ) ?></h1>
 
+    <h3><?php _e( 'Add slide', 'wfmpanel' ) ?></h3>
+
+	<?php
+    $errors = get_transient( 'wfmpanel_form_errors' );
+    $success = get_transient( 'wfmpanel_form_success' );
+    ?>
+	<?php if ( $errors ): ?>
+        <div id="setting-error-settings_updated" class="notice notice-error settings-error is-dismissible">
+            <p><strong><?php echo $errors; ?></strong></p>
+        </div>
+		<?php delete_transient( 'wfmpanel_form_errors' ) ?>
+	<?php endif; ?>
+
+	<?php if ( $success ): ?>
+        <div id="setting-error-settings_updated" class="notice notice-success settings-error is-dismissible">
+            <p><strong><?php echo $success; ?></strong></p>
+        </div>
+		<?php delete_transient( 'wfmpanel_form_success' ) ?>
+	<?php endif; ?>
+
+    <form action="<?php echo admin_url( 'admin-post.php' ) ?>" method="post" class="wfmpanel-add-slide">
+        <table class="form-table">
+            <tbody>
+
+            <tr>
+                <th>
+                    <label for="slide_title"><?php _e( 'Slide title', 'wfmpanel' ) ?></label>
+                </th>
+                <td>
+                    <input type="text" id="slide_title" class="regular-text" name="slide_title">
+                </td>
+            </tr>
+
+            <tr>
+                <th>
+                    <label for="slide_content"><?php _e( 'Slide content', 'wfmpanel' ) ?></label>
+                </th>
+                <td>
+                    <textarea name="slide_content" id="slide_content" cols="50" rows="10"
+                              class="large-text code"></textarea>
+                </td>
+            </tr>
+
+            </tbody>
+        </table>
+
+		<?php wp_nonce_field( 'wfmpanel_action', 'wfmpanel_add_slide' ) ?>
+        <input type="hidden" name="action" value="save_slide">
+
+        <p class="submit">
+            <button class="button button-primary" type="submit"
+                    id="submit"><?php _e( 'Add slide', 'wfmpanel' ) ?></button>
+        </p>
+    </form>
+
+    <hr>
+
+    <h3><?php _e( 'Edit slides', 'wfmpanel' ) ?></h3>
+
 </div>
